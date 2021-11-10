5acdf8325cdd87cebe50a45670036f2d1e92c394
Craig <craigjaredbutler@gmail.com>
2021-11-10 00:40:38 -0700

    BLSellPurACQ

--- acq_extract.py
+++ acq_extract.py
@@ -9,7 +9,6 @@ import spacy
 
 from spacy import displacy  # use to see what is each word in our sentence is corresponding to
 nlp = spacy.load("en_core_web_md")
-
 import numpy as np
 from nltk.tokenize import sent_tokenize, word_tokenize
 from spacy.util import get_package_path
@@ -23,12 +22,7 @@ from nltk import ne_chunk
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 from nltk.corpus import stopwords
-# from nltk.probability import FreqDist   #Could be used to reduce our most frequent words like: "the, a, ',', '.', "
-# fdist = FreqDist()
-# fdist_top10 = fdist.most_common(10)
-# import re
-# punctuation = compile(r'[-.?!,:;()|0-9]')
-
+nltk.download('stop_words')
 
 # Globals
 FILENAME = []
@@ -76,72 +70,70 @@ def readFiles():
     for path,file in zip(PATH, FILENAME):
         story = Story(file)
         para = []
-
+        docs = []
         #Do Stuff to get data needed
         for line in open(path + file,"r"):
             type(line)
             line = line.strip()
-            # sent_token = sent_tokenize(line)
             para_tokenize = blankline_tokenize(line)
-            # line_tokenize = word_tokenize(line)
             if not para_tokenize:   # Breaking up our paragraphs into a paragraph
                 para = []
             if para_tokenize:
                 para.extend(para_tokenize)
-            # line_tokenize = word_tokenize(line)  # Splitting up our paragraphs into single string lists
-            # if not line_tokenize:
-            #     para = []
-            # if line_tokenize:
-            #     para.extend(line_tokenize)
-
-
-
             pass
         para = ' '.join(para)
         doc = nlp(para)
-        # print (doc)
-        for ent in doc.ents:
-            # print(ent.text, ent.label_)
-            pass
-        matcher = Matcher(nlp.vocab)
-        pattern = [{"POS": "PROPN", "OP": "+", "POS": "VERB"}]
-        # pattern = [{"POS": "NOUN"}, {"POS": "NOUN"}]
-        matcher.add ("PROPER_NOUNS", [pattern], greedy = "LONGEST")
-        matches = matcher(doc)
-        print(doc)
-        for match in matches:
-            print(match, doc[match[1]:match[2]])
-        # print(matches)
-        # print(nlp.vocab[matches[0][0]].text)
+        story._purchaser = findPurchaser(doc)
+        story._acquired = findAcquired(doc)
+        story._seller = findSeller(doc)
 
-
-
-
-
-        for token in doc:
-            # print (token.text, token.pos_, token.dep_)
-            pass
-        # visulize
-        # sentence_spans = list(doc.sents)    
-        # options = {"compact": True, "bg": "#09a3d5",
-        #     "color": "white", "font": "Source Sans Pro"}
-        # displacy.serve(sentence_spans, style="dep", options=options)
-        
-
-    
 
         break
-
-
-        # words_tagged = pos_tag(para.split())
-        # chunked_tagged = ne_chunk(words_tagged)
-        # place = [word for word,pos in chunked_tagged if pos == 'GPE']
-        # print(chunked_tagged)
-
-
-        
         #append story with data
         STORIES.append(story)
+    return docs
+
+
+def findPurchaser(doc):
+    purchasers = []
+    matcher = Matcher(nlp.vocab)
+    pattern = [{"POS": "PROPN", "OP": "+"}] 
+    matcher.add ("PURCHASERS", [pattern], greedy = "LONGEST")
+    matches = matcher(doc)
+    matches.sort(key = lambda x: x[1])
+    for match in matches:
+        purchasers.append(doc[match[1]:match[2]])
+    purchaser = purchasers[0]
+    return purchaser
+
+def findAcquired(doc):
+    acquireds = []
+    matcher = Matcher(nlp.vocab)
+    pattern = [{"POS": "PROPN", "OP": "+"}] 
+    matcher.add ("PURCHASERS", [pattern], greedy = "LONGEST")
+    matches = matcher(doc)
+    matches.sort(key = lambda x: x[1])
+    for match in matches:
+        acquireds.append(doc[match[1]:match[2]])
+    if len(acquireds) > 2:
+        acquired = (acquireds[1])     
+    return acquired
+
+def findSeller(doc):
+    sellers = []
+    matcher = Matcher(nlp.vocab)
+    pattern = [{"POS": "PROPN", "OP": "+"}] 
+    matcher.add ("PURCHASERS", [pattern], greedy = "LONGEST")
+    matches = matcher(doc)
+    matches.sort(key = lambda x: x[1])
+    for match in matches:
+        sellers.append(doc[match[1]:match[2]])
+    if len(sellers) > 3:
+        seller = (sellers[2])   
+    else:
+        seller = "..."  
+    return seller
+
 
 def writeData(docList:str):
     global STORIES
@@ -151,15 +143,20 @@ def writeData(docList:str):
     
     for story in STORIES:
         print("TEXT: ", story._text, file=outFile)
-        print("ACQUIRED: ", story._acquired, file=outFile)
+        for organizations in story._acquired:
+            print("ACQUIRED: ", organizations, file=outFile)
         print("ACQBUS: ", story._acqbus, file=outFile)
         print("ACQLOC: ", story._acqloc, file=outFile)
         print("DLRAMT: ", story._dlramt, file=outFile)
-        print("PURCHASER: ", story._purchaser, file=outFile)
-        print("SELLER: ", story._seller, file=outFile)
+        for organizations in story._purchaser:
+            print("PURCHASER: ", organizations, file=outFile)
+        for organizations in story._seller:
+            print("SELLER: ", organizations, file=outFile)
         print("STATUS: ", story._status, file=outFile)
 
         outFile.write("\n")
+
+
 
 
 # Driver Section
@@ -170,7 +167,6 @@ if (len(sys.argv)) == 2:
     
 else:
     print("Please include files in the command line")
-
 
 
 
