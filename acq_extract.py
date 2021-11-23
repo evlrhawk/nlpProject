3dfdbf18537fb47bda040ba68c7c8e5a7f38401a
Craig <craigjaredbutler@gmail.com>
2021-11-23 12:16:05 -0700

    basicML

--- acq_extract.py
+++ acq_extract.py
@@ -6,7 +6,7 @@ import sys
 import nltk
 import spacy
 # python -m spacy download en_core_web_md
-
+from spacy.tokens import DocBin
 from spacy import displacy  # use to see what is each word in our sentence is corresponding to
 nlp = spacy.load("en_core_web_md")
 import numpy as np
@@ -23,12 +23,16 @@ nltk.download('maxent_ne_chunker')
 nltk.download('words')
 from nltk.corpus import stopwords
 nltk.download('stop_words')
+from spacy.training import Corpus
 
 # Globals
 FILENAME = []
 PATH = []
 STORIES = []
-
+FILENAME_ans = []
+PATH_ans = []
+STORIES_ans = []
+ans_list_list = []
 # Classes
 class Story:
     _text = "---"
@@ -66,12 +70,14 @@ def readFiles():
     global FILENAME
     global PATH
     global STORIES
-
+    global ans_list_list
+    i = 0
     for path,file in zip(PATH, FILENAME):
         story = Story(file)
         para = []
         docs = []
         #Do Stuff to get data needed
+
         for line in open(path + file,"r"):
             type(line)
             line = line.strip()
@@ -82,20 +88,56 @@ def readFiles():
                 para.extend(para_tokenize)
             pass
         para = ' '.join(para)
+
+
+        #MACHINE LEARNING
+        nlp = spacy.blank("en")
+        ner = nlp.create_pipe("ner")
+        nlp.add_pipe("ner", last=True)
+        ner = nlp.get_pipe("ner")
+        if i <= 5:
+            j = 0
+            while j < 7:
+                ent_list = ["ACQUIRED", "ACQBUS", "ACQLOC", "DLRAMT", "PURCHASER", "SELLER", "STATUS"]
+                TRAIN_DATA = [(para, {"entities": [str(ans_list_list[i][j]), str(ent_list[j])]}),]
+                db = DocBin()
+                for text, annotations in TRAIN_DATA:
+
+                    # ner.add_label(annotations[2])
+
+                # return nlp
+                    doc = nlp(text)
+                    db.add(doc)
+                db.to_disk("./train.spacy")  
+                j = j +1
+
+
         doc = nlp(para)
-        story._purchaser = findPurchaser(doc)
-        story._acquired = findAcquired(doc)
-        story._seller = findSeller(doc)
+        if i > 5:
+            for ent in doc:
+                print(ent.text, ent.label_)  # what label corresponds to the text
+                pass
 
 
-        break
+
+
+    #PATTERNS
+
+        # doc = nlp(para)
+        # story._purchaser = findPurchaser(doc)
+        # story._acquired = findAcquired(doc)
+        # story._seller = findSeller(doc)
+
+
         #append story with data
         STORIES.append(story)
-    return docs
+        i = i+1
+    return
 
 
 def findPurchaser(doc):
     purchasers = []
+    purchase = []
     matcher = Matcher(nlp.vocab)
     pattern = [{"POS": "PROPN", "OP": "+"}] 
     matcher.add ("PURCHASERS", [pattern], greedy = "LONGEST")
@@ -106,10 +148,16 @@ def findPurchaser(doc):
     purchaser = purchasers[0]
     return purchaser
 
+
+
 def findAcquired(doc):
     acquireds = []
+    acquire = []
     matcher = Matcher(nlp.vocab)
     pattern = [{"POS": "PROPN", "OP": "+"}] 
+    # pattern = [{"LEMMA": {"IN": ["sale", "buy", "purchase", "acquire", "get", "obtain", "take", "secure", "gain", "procure", "trade"]}}]
+    # pattern = [{"ENT_TYPE": "ORG"}]
+    # pattern = [{{"POS": "PROPN", "OP": "+"}, {"-"}, {"POS": "PROPN", "OP": "+"}}]
     matcher.add ("PURCHASERS", [pattern], greedy = "LONGEST")
     matches = matcher(doc)
     matches.sort(key = lambda x: x[1])
@@ -123,6 +171,9 @@ def findSeller(doc):
     sellers = []
     matcher = Matcher(nlp.vocab)
     pattern = [{"POS": "PROPN", "OP": "+"}] 
+    # pattern = [{"LEMMA": {"IN": ["sale", "buy", "purchase", "acquire", "get", "obtain", "take", "secure", "gain", "procure", "trade"]}}]
+    # pattern = [{"ENT_TYPE": "ORG"}]
+    # pattern = [{{"POS": "PROPN", "OP": "+"}, {"-"}, {"POS": "PROPN", "OP": "+"}}]
     matcher.add ("PURCHASERS", [pattern], greedy = "LONGEST")
     matches = matcher(doc)
     matches.sort(key = lambda x: x[1])
@@ -142,32 +193,69 @@ def writeData(docList:str):
     outFile = open(fileName, "w")
     
     for story in STORIES:
-        print("TEXT: ", story._text, file=outFile)
-        for organizations in story._acquired:
-            print("ACQUIRED: ", organizations, file=outFile)
-        print("ACQBUS: ", story._acqbus, file=outFile)
-        print("ACQLOC: ", story._acqloc, file=outFile)
-        print("DLRAMT: ", story._dlramt, file=outFile)
-        for organizations in story._purchaser:
-            print("PURCHASER: ", organizations, file=outFile)
-        for organizations in story._seller:
-            print("SELLER: ", organizations, file=outFile)
-        print("STATUS: ", story._status, file=outFile)
+        # print("TEXT: ", story._text, file=outFile)
+        # print("ACQUIRED: ", story._acquired)
+        # print("ACQBUS: ", story._acqbus, file=outFile)
+        # print("ACQLOC: ", story._acqloc, file=outFile)
+        # print("DLRAMT: ", story._dlramt, file=outFile)
+        # print("PURCHASER: ", story._purchaser)
+        # print("SELLER: ", story._seller)
+        # print("STATUS: ", story._status, file=outFile)
 
         outFile.write("\n")
 
 
 
+## Test and Train
+def getFiles_ans(anslist:str):
+    global FILENAME_ans
+    global PATH_ans
+    # print(anslist)
+    for line in open(anslist, "r"):
+        newLine = line.strip()
+        # print(newLine)
+        path_ans, file_ans = newLine.rsplit("/",1)
+        path_ans += "/"
+        PATH_ans.append(path_ans)
+        FILENAME_ans.append(file_ans)
+
+def readFiles_ans():
+    global FILENAME_ans
+    global PATH_ans
+    global STORIES_ans
+    global ans_list_list
+ 
+    for path_ans,file_ans in zip(PATH_ans, FILENAME_ans):
+        story_ans = Story(file_ans)
+        para_ans = []
+        docs_ans = []
+
+        first = []
+        i = 0
+        ans_list = []
+        for line in open(path_ans + file_ans,"r"):
+
+            line = str(line.strip())
+            first = line.split()
+            first = first[1:]
+            first = ' '.join(first)
+            if i > 0:
+                if first:
+                    first = first.replace('"','')
+                    ans_list.append(first)
+            i = i+1
+        
+        ans_list_list.append(ans_list)
+
+
+
+
 
 # Driver Section
-if (len(sys.argv)) == 2:
-    getFiles(sys.argv[1])
+if (len(sys.argv)) == 3:
+    getFiles_ans(sys.argv[1])
+    getFiles(sys.argv[2])
+    readFiles_ans()
     readFiles()
-    writeData(sys.argv[1])
+    writeData(sys.argv[2])
     
-else:
-    print("Please include files in the command line")
-
-
-
-
